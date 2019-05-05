ed9292ab25f6397f56210542779bf22966f20b28
PEDRO BEETHOVEN DA COSTA <pedro.beethoven@academico.ifrn.edu.br>
2019-05-05 14:27:12 -0300

    remoção e busca adicionados

--- Estrutura_de_dados/src/arvore_binaria/ArvoreBinaria.java
+++ Estrutura_de_dados/src/arvore_binaria/ArvoreBinaria.java
@@ -1,6 +1,7 @@
 package arvore_binaria;
 
 import java.util.ArrayList;
+
 import arvore_binaria.Node;
 
 public class ArvoreBinaria{
@@ -42,13 +43,23 @@ public class ArvoreBinaria{
 	}
 
 	public ArrayList<Object> elements() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Object> elementos = new ArrayList<Object>();
+		ArrayList<Node> preOrderAux = this.preOrder(this.raiz, false);
+		int tam = preOrderAux.size();
+		for(int i = 0; i < tam; i++) {
+			elementos.add(preOrderAux.get(i).getElemento());
+		}
+		return elementos;
 	}
 
 	public ArrayList<Node> nos() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Node> nos = new ArrayList<Node>();
+		ArrayList<Node> preOrderAux = this.preOrder(this.raiz, false);
+		int tam = preOrderAux.size();
+		for(int i = 0; i < tam; i++) {
+			nos.add(preOrderAux.get(i));
+		}
+		return nos;
 	}
 
 	public Node root() {
@@ -56,14 +67,17 @@ public class ArvoreBinaria{
 	}
 
 	public Node parent(Node filho) {
-		// TODO Auto-generated method stub
-		return null;
+		return filho.getPai();
 	}
 
 	public ArrayList<Node> children(Node pai) {
 		ArrayList<Node> filhos = new ArrayList<Node>();
-		filhos.add(pai.getEsquerdo());
-		filhos.add(pai.getEsquerdo());
+		if(pai.getEsquerdo() != null) {
+			filhos.add(pai.getEsquerdo());
+		}
+		if(pai.getDireito() != null) {
+			filhos.add(pai.getEsquerdo());
+		}
 		return filhos;
 	}
 
@@ -85,9 +99,16 @@ public class ArvoreBinaria{
 		else return 1 + depth(no.getPai());
 	}
 
-	public Object replace(Node no, Object o) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object replace(Node no, int v) { //precisa consertar
+		int old = no.getElemento();
+		no.setElemento(v);
+		return old;
+	}
+	
+	public void swapElements(Node no1, Node no2) { //precisa consertar
+		int aux = no1.getElemento();
+		no1.setElemento(no2.getElemento());
+		no2.setElemento(aux);
 	}
 
 	public void insert(Node no, int valor) {
@@ -117,8 +138,56 @@ public class ArvoreBinaria{
 	}
 
 	public Object remove(Node no) {
-		// TODO Auto-generated method stub
-		return null;
+		int valor = 0;
+		//folha
+		if(this.isExternal(no)) {
+			valor = no.getElemento();
+			if(no.getPai().getEsquerdo() == no) {
+				no.getPai().setEsquerdo(null);
+			}
+			else {
+				no.getPai().setDireito(null);
+			}
+		}
+		else {
+			int qtdF = this.children(no).size();
+			//1 filho
+			if(qtdF == 1) {
+				valor = no.getElemento();
+				if(no.getPai().getDireito() == no) { //eh o filho direito do pai
+					if(no.getEsquerdo() != null) { //so tem filho na esquerda
+						no.getPai().setDireito(no.getEsquerdo());
+						no.getEsquerdo().setPai(no.getPai());
+					}
+					else { //so tem filho na direita
+						no.getPai().setDireito(no.getDireito());
+						no.getDireito().setPai(no.getPai());
+					}
+				}
+				else {
+					if(no.getEsquerdo() != null) { //so tem filho na esquerda
+						no.getPai().setEsquerdo(no.getEsquerdo());
+						no.getEsquerdo().setPai(no.getPai());
+					}
+					else { //so tem filho na direita
+						no.getPai().setEsquerdo(no.getDireito());
+						no.getDireito().setPai(no.getPai());
+					}
+				}
+			}
+			else { //2 filhos
+				valor = no.getElemento();
+				Node aux = no.getEsquerdo();
+				Node sub = null; //o substituto do nó que sera removido
+				while(aux != null) {
+					sub = aux; //nó mais a direita da sub-arvore esquerda
+					aux = aux.getDireito();
+				}
+				no.setElemento(sub.getElemento());
+				this.remove(sub);
+			}
+		}
+		return valor;
 	}
 	
 	public ArrayList<Node> preOrder(Node no, boolean visitado) {
@@ -154,21 +223,24 @@ public class ArvoreBinaria{
 			listaPosOrder = new ArrayList<Node>();
 		}
 		if(no.getEsquerdo() != null) {
-			inOrder(no.getEsquerdo(), true);
+			posOrder(no.getEsquerdo(), true);
 		}
 		if(no.getDireito() != null) {
-			inOrder(no.getDireito(), true);
+			posOrder(no.getDireito(), true);
 		}
 		listaPosOrder.add(no);
-		return listaPosOrder;
+		if(!visitado) {
+			return listaPosOrder;
+		}
+		return null;
 	}
 	
 	public String mostraArvore() {
 		String arvoreCompleta = "";
 		
-		ArrayList<Node> tmpList = this.inOrder(this.raiz, false);
+		ArrayList<Node> nos = this.inOrder(this.raiz, false);
 		
-		int largura = tmpList.size();
+		int largura = nos.size();
 		int altura = this.height(this.raiz);
 		
 		String[][] arvore = new String[altura+1][largura];
@@ -177,7 +249,7 @@ public class ArvoreBinaria{
 		int l = 0;
 		
 		for(int i=0; i<largura; i++) {
-			no = tmpList.get(i);
+			no = nos.get(i);
 			l = this.depth(no);
 			
 			arvore[l][i] = String.valueOf(no.getElemento());
@@ -186,16 +258,49 @@ public class ArvoreBinaria{
 		for(int i=0; i<altura+1; i++) {
 			for(int j=0; j<largura; j++) {
 				if(arvore[i][j] == null) {
-					arvoreCompleta += " ";
+					arvoreCompleta += "  ";
 				}
 				else {
 					
-					arvoreCompleta += arvore[i][j]+" ";
+					arvoreCompleta += arvore[i][j];
 				}
 			}
 			arvoreCompleta += "\n";
 		}
 		return arvoreCompleta;
 	}
+	
+	public Node search(int valor, Node no) {
+		Node aux = null;
+		if(valor == no.getElemento()) {
+			return no;
+		}
+		if(valor < no.getElemento()) {
+			aux = this.search(valor, no.getEsquerdo());
+		}
+		if(valor > no.getElemento()) {
+			aux =this.search(valor, no.getDireito());
+		}
+		no = aux;
+		return no;
+	}
+	
+	public Node leftChild(Node no) {
+		return no.getEsquerdo();
+	}
+	
+	public Node rightChild(Node no) {
+		return no.getDireito();
+	}
+	
+	public boolean hasLeftChild(Node no) {
+		return (no.getEsquerdo() != null)?true:false;
+	}
+	
+	public boolean hasRightChild(Node no) {
+		return (no.getDireito() != null)?true:false;
+	}
+	
+	
 
 }
--- Estrutura_de_dados/src/arvore_binaria/MainArvoreBP.java
+++ Estrutura_de_dados/src/arvore_binaria/MainArvoreBP.java
@@ -6,20 +6,24 @@ public class MainArvoreBP {
 		
 		ArvoreBinaria bt = new ArvoreBinaria();
 		
-		bt.insert(null, 10);
+		bt.insert(null, 20);
+		bt.insert(bt.root(), 1000);
 		bt.insert(bt.root(), 15);
-		bt.insert(bt.root(), 4);
-		//bt.preOrder(bt.root());
-		bt.insert(bt.root(), 2);
-		bt.insert(bt.root(), 20);
-		//bt.insert(bt.root(), 21);
-		//bt.insert(bt.root(), 19);
-//		for(Node e : bt.inOrder(bt.root(), false)) {
-//			System.out.print(e.getElemento() + " ");
-//		}
-		//System.out.println(bt.mostrarArvore());
+		bt.insert(bt.root(), 32);
+		bt.insert(bt.root(), 25);
+		bt.insert(bt.root(), 11);
+		bt.insert(bt.root(), 16);
+		bt.insert(bt.root(), 12);
+		bt.insert(bt.root(), 2000);
 		System.out.println(bt.mostraArvore());
-		System.out.println("altura: "+ bt.height(bt.root()));
+		//bt.replace(bt.root().getDireito(), 5000);
+		//System.out.println(bt.mostraArvore());
+		bt.remove(bt.search(15, bt.root()));
+		System.out.println(bt.mostraArvore());
+		bt.remove(bt.search(32, bt.root()));
+		System.out.println(bt.mostraArvore());
+		bt.remove(bt.search(1000, bt.root()));
+		System.out.println(bt.mostraArvore());
 	}
 
 }
